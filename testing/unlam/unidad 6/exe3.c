426559c8c9e903e996a74bb7193c75993b1e8f87
qwertynicolas <qwertynicolas@proton.me>
2024-06-25 14:15:58 -0300

    Delete testing/unlam/unidad 6/exe3.c

--- testing/unlam/unidad 6/exe3.c
+++
@@ -1,7 +0,0 @@
-/* Ejercicio 3: Números Primos en un Rango
-
-Escribe un programa que lea números enteros en un rango de 10 a 100 (usa una función LeerYValidar). El programa debe:
-
-    Contar cuántos números primos se ingresan.
-    Terminar cuando se ingrese el número 10. */
-
